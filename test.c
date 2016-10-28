af01e4f5edb13293b6f3679c7685640bb572248d
Job Talle <jobtalle@hotmail.com>
2016-10-28 13:59:44 +0200

    Update test.c

--- test.c
+++ test.c
@@ -13,23 +13,25 @@ int  test_pool(int element_size, int block_size)
 	int *test_ptr1 = NULL;
 	int *test_ptr2 = NULL;
 	
-	/* init mem pool of given args */
-	poolInitialize(&pool_ptr,element_size, block_size);
+	/* init memory pool with given parameters */
+	poolInitialize(&pool_ptr, element_size, block_size);
 	
 	/* allocate memory from memory pool */
 	test_ptr1 = poolMalloc(&pool_ptr);
 	test_ptr2 = poolMalloc(&pool_ptr);
 	
+	/* test allocated memory validity */
 	if(!(test_ptr1 && test_ptr2))
 	{
 		printf("memory allocation failure \n");
+		
 		return FAILURE;
 	}
 	
 	/* free memory allocated from memory pool */
-	 poolFree(&pool_ptr, test_ptr1);
+	poolFree(&pool_ptr, test_ptr1);
 	
-	/* free all memory used from this pool */
+	/* free all memory used by this pool */
 	poolFreePool(&pool_ptr);
 	
 	return SUCCESS;
@@ -37,18 +39,17 @@ int  test_pool(int element_size, int block_size)
 
 int main()
 {
+	if(test_pool(4, 8) == FAILURE)
+		printf("test_pool failure %s %d \n", __FILE__, __LINE__);
 	
-	if(test_pool(4,8) == FAILURE)
-		printf("test_pool failure %s %d \n",__FILE__,__LINE__);
+	if(test_pool(8, 8) == FAILURE)
+		printf("test_pool failure %s %d \n", __FILE__, __LINE__);
 	
-	if(test_pool(8,8) == FAILURE)
-		printf("test_pool failure %s %d \n",__FILE__,__LINE__);
-	
-	if(test_pool(16,8) == FAILURE)
-		printf("test_pool failure %s %d \n",__FILE__,__LINE__);
+	if(test_pool(16, 8) == FAILURE)
+		printf("test_pool failure %s %d \n", __FILE__, __LINE__);
 
-	if(test_pool(32,8) == FAILURE)
-		printf("test_pool failure %s %d \n",__FILE__,__LINE__);
+	if(test_pool(32, 8) == FAILURE)
+		printf("test_pool failure %s %d \n", __FILE__, __LINE__);
   
 
 	return SUCCESS;
